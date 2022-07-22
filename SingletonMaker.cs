278ccc81dc4ac2973d90adcd14d074f7d83f908b
Stathis Papadopoulos <stathisthemagic@gmail.com>
2022-07-23 02:24:08 +0300

    Initial Commit

---
+++ SingletonMaker.cs
@@ -0,0 +1,16 @@
+﻿namespace Statharas.Tools;
+public static class SingletonMaker
+{
+	static Dictionary<Type, object> dict = new();
+
+	public static T Singleton<T>() where T : new()
+	{
+
+		dict.TryGetValue(typeof(T), out var value);
+		if (value is not null) return (T)value;
+		//if (dict.ContainsKey(typeof(T))) return (T)dict[typeof(T)];
+		var z = new T();
+		dict.Add(typeof(T), z);
+		return z;
+	}
+}
